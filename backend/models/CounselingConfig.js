daa3a5fed512c48818d808e356bdf382dc73a29d
Vicky8104 <vkjangid8104@gmail.com>
2026-09-05 06:18:01 +0530

    Update CounselingConfig.js

--- backend/models/CounselingConfig.js
+++ backend/models/CounselingConfig.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
-const configSchema  = new mongoose.Schema({
+
+const configSchema = new mongoose.Schema({
   post: String,
   area: String,
   subject: String,
   startDate: Date,
   endDate: Date,
   isActive: { type: Boolean, default: true }
-}, {timestamps:true});
+}, { timestamps: true });
+
+
+// INDEX
+configSchema.index({
+  post: 1,
+  area: 1,
+  subject: 1
+});
+
 
 export default mongoose.model("CounselingConfig", configSchema);
